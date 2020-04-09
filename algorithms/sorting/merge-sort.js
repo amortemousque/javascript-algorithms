a1434f5a29cf2945de16b9fac1f6395fc1dfb211
Aymeric <aymeric@mortemousque.com>
2020-04-09 11:42:58 +0200

    add merge sort and quick sort

--- algorithms/sorting/merge-sort.js
+++ algorithms/sorting/merge-sort.js
@@ -1,50 +1,3 @@
-
-// Worst-case performance	O(n log n)
-// Best-case performance	O(n log n) typical, O(n) natural variant
-// Average performance	O(n log n)
-// Worst-case space complexity	О(n) total with O(n) auxiliary, O(1) auxiliary with linked lists[1]
-import center from 'center-align'
-
-
-let splitting = [];
-let merging = [];
-
-function addDrawingSNode(arr, level) {
-    if (!splitting[level]) {
-        splitting[level] = [arr]
-    } else {
-        splitting[level].push(arr)
-    }
-}
-
-function addDrawingMNode(arr, level) {
-    if (!merging[level]) {
-        merging[level] = [arr]
-    } else {
-        merging[level].push(arr);
-    }
-}
-
-function draw() {
-    let output = []
-    splitting.forEach(line => {
-        line = line.map(arr => '[' + arr.map(v => `${v}`) + ']')
-        output.push(line.join(' '))
-    })
-
-    merging.reverse().forEach(line => {
-        line = line.map(arr => '[' + arr.map(v => `${v}`) + ']')
-        output.push(line.join(' '))
-    })
-    let clock = 0
-    let delay = 1000
-
-    center(output).forEach(l => {
-        clock += delay
-        setTimeout(() => { console.log(l) }, clock);
-    })
-}
-
 function merge(left, right) {
     let i = 0
     let j = 0
@@ -52,37 +5,25 @@ function merge(left, right) {
     while (i < left.length && j < right.length) {
         if (left[i] < right[j]) {
             arr.push(left[i])
-            i++;
+            i++
         } else {
-            arr.push(right[j]);
-            j++;
+            arr.push(right[j])
+            j++
         }
     }
 
-
-    arr = arr.concat(left.slice(i), (right.slice(j)))
-    return arr;
+    return arr.concat(left.slice(i), right.slice(j));
 }
-export default function mergeSort(arr, level = 0) {
 
-    level++;
-    addDrawingSNode(arr, level)
+export default function mergeSort(arr) {
 
     if (arr.length < 2)
         return arr;
 
-
-    let middle = Math.floor(arr.length / 2);
-    let left = arr.slice(0, middle);
+    let middle = Math.floor(arr.length / 2)
+    let left = arr.slice(0, middle)
     let right = arr.slice(middle)
 
-    let merged = merge(mergeSort(left, level), mergeSort(right, level));
-    addDrawingMNode(merged, level)
-
-    if (level == 1) {
-        draw()
-    }
-
-    return merged
+    return merge(await mergeSort(left, level), await mergeSort(right))
 }
 
